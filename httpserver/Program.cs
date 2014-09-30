a6ea589d95723ca69907e65c53169d27005e693e
ChristianFranklin <roang@live.dk>
2014-09-30 15:36:09 +0200

    Started på 1a

--- httpserver/Program.cs
+++ httpserver/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Configuration;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +12,32 @@ namespace httpserver
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello http server");
-        }
+         public static void Main(string[] args)
+     {  
+        HttpListener serverSocket = new HttpListener(80);
+        serverSocket.Start();
 
+        Stream ns = connectionSocket.GetStream();
+        // Stream ns = new NetworkStream(connectionSocket);
+
+    
+        StreamReader sr = new StreamReader(ns);
+        StreamWriter sw = new StreamWriter(ns);
+        sw.AutoFlush = true; 
+ 
+
+
+
+
+
+
+        Console.WriteLine("Hello http server");
+     }
         
+           
+        
+
+         
     }
 
 }
