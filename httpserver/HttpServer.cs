6e3ebd353ed6e9b7fc63bb713bea2ded79d1ebca
ChristianFranklin <roang@live.dk>
2014-10-02 17:14:28 +0200

    Ryddet op i kode

--- httpserver/HttpServer.cs
+++ httpserver/HttpServer.cs
@@ -16,22 +16,19 @@ namespace httpserver
         private FileStream fs;
 
 
-
-
         public HttpServer(TcpClient connectionSocket)
         {
 
             const string rn = "\r\n";
 
-            //Socket connectionSocket = serverSocket.AcceptSocket();
             Console.WriteLine("Connected: {0}");
 
             NetworkStream ns = connectionSocket.GetStream();
-            // Stream ns = new NetworkStream(connectionSocket);
+            
 
             StreamReader sr = new StreamReader(ns);
             StreamWriter sw = new StreamWriter(ns);
-            sw.AutoFlush = true; // enable automatic flushing
+            sw.AutoFlush = true; 
 
 
             string[] sArray = new string[3];
@@ -42,53 +39,33 @@ namespace httpserver
             Console.WriteLine(sArray.GetValue(1));
             findfil(sArray.GetValue(1).ToString(), ns);
 
-            //request
             Console.WriteLine("Client: " + message);
             message = sr.ReadLine();
             Console.WriteLine(message);
-            //response
-
 
             using (fs = File.OpenRead(RootCatalog))
             {
-                
+                string answer = "HTTP/1.0 200 OK\r\n\r\nHello Payam!" + rn + "Hello Christian!";
+                sw.WriteLine(answer);
+                Console.WriteLine(answer);
 
-            string answer = "HTTP/1.0 200 OK\r\n\r\nHello Payam!" + rn + "Hello Christian!";
-            sw.WriteLine(answer);
-            Console.WriteLine(answer);
-                
                 fs.CopyTo(sw.BaseStream);
                 sw.BaseStream.Flush();
                 sw.Flush();
             }
-            
 
-          
             ns.Close();
             connectionSocket.Close();
-            }
+        }
 
 
-       
-
-            public void findfil(string filnavn, NetworkStream network)
-            
-                 
+        public void findfil(string filnavn, NetworkStream network)
+        {
+            if (File.Exists(RootCatalog + filnavn))
             {
-                if (File.Exists(RootCatalog + filnavn))
-                {
-                    FileStream fs = File.OpenRead(RootCatalog + filnavn);
-                    fs.CopyTo(network);
-                }
-
-
-               
+                FileStream fs = File.OpenRead(RootCatalog + filnavn);
+                fs.CopyTo(network);
             }
-
-  
-
-
-
-
-}
+        }
+    }
 }
